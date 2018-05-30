2a5ad403db54bafad22ea85326651c499d429f96
selvarakeshkumar9865@gmail.com <39699212+selvarakeshkumar@users.noreply.github.com>
2018-05-30 12:43:11 +0530

    Add files via upload

---
+++ bre.java
@@ -0,0 +1,22 @@
+package datatype;
+
+public class bre {
+	
+	
+	public static void main(String[] args) {
+	int i;
+	for(i=0;i<10;i++) {
+		
+		if(i<3) {
+			System.out.println("break loop i= "+i);
+			continue;
+				}
+		if (i<=5) {
+			System.out.println("break loop i= ");
+			break;
+		}
+		
+		}
+	}
+
+}
